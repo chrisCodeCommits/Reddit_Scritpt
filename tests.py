8e72ab691c44f8c75f9facc9f1ad1b4ba5d10837
chris <chrispyprogrammer@gmail.com>
2018-10-26 16:14:32 +0200

    function to call reddit API with specific passed subreddit as argument

--- tests.py
+++ tests.py
@@ -1,7 +1,8 @@
 '''
 TASK:
 A list of Subreddits and search terms
-A function to call Reddit's API (pass in subreddit)
+///////A function to call Reddit's API (pass in subreddit)
+
 A function to process reddit's results (search for search terms, returns positives)
 A function that calls reddit's API for each subreddit in our list
 A function that calls Twilios API to send a Text with matching results
