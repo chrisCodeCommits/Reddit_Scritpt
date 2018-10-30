8913fb6ea10729a2aad4ddf25c6acd227b736e38
chris <chrispyprogrammer@gmail.com>
2018-10-30 15:49:33 +0200

    assigning and testing variables/pre adding a conditional statment

--- src/sw_api_script.py
+++ src/sw_api_script.py
@@ -8,7 +8,6 @@ in my code.
 STEP 2
 parse the dictionary to output:
 - All male characters names
-- Date of birth
 - Species
 '''
 ##################################################################
@@ -19,7 +18,7 @@ sw_api_request = {
    "next":"https://swapi.co/api/people/?page=2&format=json",
    "previous":"null",
    "results":[
-      {
+       {
          "name":"Luke Skywalker",
          "height":"172",
          "mass":"77",
@@ -50,6 +49,98 @@ sw_api_request = {
          "created":"2014-12-09T13:50:51.644000Z",
          "edited":"2014-12-20T21:17:56.891000Z",
          "url":"https://swapi.co/api/people/1/"
+      },
+      {
+         "name":"C-3PO",
+         "height":"167",
+         "mass":"75",
+         "hair_color":"n/a",
+         "skin_color":"gold",
+         "eye_color":"yellow",
+         "birth_year":"112BBY",
+         "gender":"n/a",
+         "homeworld":"https://swapi.co/api/planets/1/",
+         "films":[
+            "https://swapi.co/api/films/2/",
+            "https://swapi.co/api/films/5/",
+            "https://swapi.co/api/films/4/",
+            "https://swapi.co/api/films/6/",
+            "https://swapi.co/api/films/3/",
+            "https://swapi.co/api/films/1/"
+         ],
+         "species":[
+            "https://swapi.co/api/species/2/"
+         ],
+         "vehicles":[
+
+         ],
+         "starships":[
+
+         ],
+         "created":"2014-12-10T15:10:51.357000Z",
+         "edited":"2014-12-20T21:17:50.309000Z",
+         "url":"https://swapi.co/api/people/2/"
+      },
+      {
+         "name":"R2-D2",
+         "height":"96",
+         "mass":"32",
+         "hair_color":"n/a",
+         "skin_color":"white, blue",
+         "eye_color":"red",
+         "birth_year":"33BBY",
+         "gender":"n/a",
+         "homeworld":"https://swapi.co/api/planets/8/",
+         "films":[
+            "https://swapi.co/api/films/2/",
+            "https://swapi.co/api/films/5/",
+            "https://swapi.co/api/films/4/",
+            "https://swapi.co/api/films/6/",
+            "https://swapi.co/api/films/3/",
+            "https://swapi.co/api/films/1/",
+            "https://swapi.co/api/films/7/"
+         ],
+         "species":[
+            "https://swapi.co/api/species/2/"
+         ],
+         "vehicles":[
+
+         ],
+         "starships":[
+
+         ],
+         "created":"2014-12-10T15:11:50.376000Z",
+         "edited":"2014-12-20T21:17:50.311000Z",
+         "url":"https://swapi.co/api/people/3/"
+      },
+      {
+         "name":"Darth Vader",
+         "height":"202",
+         "mass":"136",
+         "hair_color":"none",
+         "skin_color":"white",
+         "eye_color":"yellow",
+         "birth_year":"41.9BBY",
+         "gender":"male",
+         "homeworld":"https://swapi.co/api/planets/1/",
+         "films":[
+            "https://swapi.co/api/films/2/",
+            "https://swapi.co/api/films/6/",
+            "https://swapi.co/api/films/3/",
+            "https://swapi.co/api/films/1/"
+         ],
+         "species":[
+            "https://swapi.co/api/species/1/"
+         ],
+         "vehicles":[
+
+         ],
+         "starships":[
+            "https://swapi.co/api/starships/13/"
+         ],
+         "created":"2014-12-10T15:18:20.704000Z",
+         "edited":"2014-12-20T21:17:50.313000Z",
+         "url":"https://swapi.co/api/people/4/"
       }
       ]
       }
@@ -58,10 +149,18 @@ sw_api_request = {
 
 swapi_people = sw_api_request["results"]
 
-print(swapi_people)
+#print(swapi_people)
 
 
+name = swapi_people[0]['name']
+specie = swapi_people[0]['species'][0]
+gender = swapi_people[0]['gender']
 
+#print("")
+#print("Name: "+name+" | "+"Gender: male"+" | "+"Specie: "+specie)
+#print("")
+
+print(gender)
 
 
 
